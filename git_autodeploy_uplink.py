b33768417a4273f44eee489bf8cead5a6c202775
Aladeusi, Olugbenga <olakunleolugbenga@gmail.com>
2019-08-25 22:48:14 +0100

    2019-08-25 22:48:14.426000

--- git_autodeploy_uplink.py
+++ git_autodeploy_uplink.py
@@ -3,23 +3,32 @@ This component of "autodeploy" dev-ops deployment pipeline
 pushes your projects, sources or libraries to github from 
 your local machine on schedule basis.
 """
-from shell import shell
-import subprocess
-import os
 from toolbox import tb_shell 
+import datetime
+
 
 
 config={
-    "repo_origin":"",
+    "repo_origin":"https://github.com/Aladeusi/python_algorithm_test_cases.git",
     "repo_local_path":"C:\\inetpub\python\\algorithms\\python_algorithm_test_cases",
     "account_username":"Aladeusi",
     "account_password":"Alad3iu5!"
 }
 
-def uplink():
-    res = tb_shell("")
+def is_not_staged(res):
+   status=True if "what will be committed" in res else False
+   return status
 
-res = git status
-if 'untracked files present' in res:
-    res=git add --all & git push origin master
-    
+def yet_to_commit(res):
+   status=True if "Changes to be committed" in res else False
+   return status
+
+def uplink():
+    res = tb_shell("cd "+config["repo_local_path"]+" & git status")
+    if is_not_staged(res) or yet_to_commit(res):
+        push_command="cd "+config["repo_local_path"]+" & git add --all & git commit -m '"+ str(datetime.datetime.now())+"' & git push "+config['repo_origin']+" master"
+        push_res=tb_shell(push_command)
+        
+# deploy repo specified in config        
+uplink()
+
